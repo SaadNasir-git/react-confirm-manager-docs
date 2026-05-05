e26f03c493335059e4623aa75f9272d3d239c620
Saad Nasir <mn0871590@gmail.com>
2026-05-05 17:12:59 +0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/react-confirm-manager/',
+  base: '/react-confirm-manager-docs/',
   plugins: [
     react(),
     tailwindcss()
